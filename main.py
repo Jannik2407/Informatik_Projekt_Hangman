64edb409837f7eb8087f2cf988755ec9e9e5785d
Jannik Ast <ast.jannik-it25@it.dhbw-ravensburg.de>
2026-03-18 13:43:57 +0100

    Added header in main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+'''
+/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+Projektname:        Hangman Spiel
+Authoren:           Jannik Ast und Tobias Brechtenbreiter
+Datum:              18. März 2026
+
+Kurzbeschreibung:   Ein Textbasiertes Hangman-Spiel in Python.
+                    Der Spieler kann zwischen drei  Schwierigkeitsgraden wählen und muss ein zufällig ausgewähltes Wort
+                    Buchstabe für Buchstabe erraten.
+                    Der Spieler hat 6 Möglichkeiten das Wort zu erraten, bevor das Spiel beendet wird.
+                    Das Spiel zeigt den Galgen als ASCII-Art, informiert über richtig und flasch geratene Buchstaben
+                    und führt eine Liste der geratenen Buchstaben.
+/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+'''
+
+
 from game_logic import play_game                                                                                            ## Importiert die Spiellogik-Funktion
 
 def main_menu():                                                                                                            ## Endlosschleife für das Hauptmenü
